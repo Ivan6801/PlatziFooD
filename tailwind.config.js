74bd04949daf0414a42fb10ceabad79d0eccfbca
Ivan6801 <hal308366@gmail.com>
2020-07-24 22:33:56 -0700

    Proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#f3c614',
+                'secondary': '#353535'
+            },
+            height: {
+                '80': '20rem'
+            } 
+        }
+    },
+    variants: {
+        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    },
+    plugins: []
+}
